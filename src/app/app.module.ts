f5478d2ee7a5ccad15ef23d3c09aeda91405e5d8
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-08-13 11:27:55 -0300

    importando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
