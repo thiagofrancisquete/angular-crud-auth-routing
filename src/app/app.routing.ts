ba3dabf48786d929c14ed5df39d65eb0eda8f46a
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-09-26 20:33:51 -0300

    rota como constante

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const APP_ROUTES: Routes = [
-
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'notFound', component: NotFoundComponent },
+  { path: '', component: HomeComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
