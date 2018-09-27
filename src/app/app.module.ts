93833405e2a06863eae93d2dbdb12285291333ac
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-09-26 21:16:43 -0300

    refatorando arquivos module com modularizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
+// import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosModule } from './cursos/cursos.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
     BrowserModule,
     MaterializeModule,
+    CursosModule,
+    AppRoutingModule
+    // routing <-- constante da rota sem modulo
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
