2267454d44f026f7d42c2bafdea4e45b2ce4c3fd
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-09-28 20:41:56 -0300

    refatoração das rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,16 +1,10 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 const APP_ROUTES: Routes = [
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'notFound', component: NotFoundComponent },
   { path: '', component: HomeComponent }
 ];
 
