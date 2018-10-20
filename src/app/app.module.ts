c93d4b65afe195905f6bde708b7cf43ab37755fc
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-20 20:22:53 -0300

    reset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { MaterializeModule } from 'angular2-materialize';
 
 // import { routing } from './app.routing';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { CursosModule } from './cursos/cursos.module';
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     MaterializeModule,
-    CursosModule,
-    AppRoutingModule
     // routing <-- constante da rota sem modulo
   ],
   providers: [],
