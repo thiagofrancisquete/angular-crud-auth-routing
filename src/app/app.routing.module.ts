34b602ea9a0e9d99300cb354aa18996c4cfcb752
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-21 19:00:24 -0300

    refatoracao: rotas principais

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+
 import { LoginComponent } from './login/login.component';
-import { CursoNotFoundComponent } from './cursos/curso-not-found/curso-not-found.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent},
   { path: 'login', component: LoginComponent},
-  { path: '404', component: CursoNotFoundComponent},
   { path: '', component: HomeComponent}
 ];
 
