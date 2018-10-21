198517f75925a19ebba13f421126a2cb1bbe6516
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-21 17:00:47 -0300

    serviço dos cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
 // import { routing } from './app.routing';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNotFoundComponent } from './curso-not-found/curso-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    MaterializeModule,
-    // routing <-- constante da rota sem modulo
-  ],
-  providers: [],
+  declarations: [AppComponent, LoginComponent, HomeComponent, CursosComponent, CursoDetalheComponent, CursoNotFoundComponent],
+  imports: [BrowserModule, MaterializeModule, AppRoutingModule], // routing
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
 
-  constructor() { }
+  getCursos() {
+    return [{ nome: 'Angular', id: 1 }, { nome: 'React', id: 2 }];
+  }
+
+  getCurso(id: number) {
+    let cursos = this.getCursos();
+    for (let i = 0; i < cursos.length; i++) {
+      let curso = cursos[i];
+      if (curso.id == id){
+        return curso;
+      } 
+    } 
+    return null;
+  }
+
+  constructor() {}
+
 }
