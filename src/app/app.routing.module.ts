13b38d1e0f14e7208a358df152a4d9dab49ddbe3
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-21 17:00:23 -0300

    modularizando as rotas

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { LoginComponent } from './login/login.component';
+import { CursoNotFoundComponent } from './curso-not-found/curso-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent},
+  { path: 'login', component: LoginComponent},
+  { path: '404', component: CursoNotFoundComponent},
+  { path: '', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
