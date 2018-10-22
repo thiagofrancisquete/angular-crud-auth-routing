7e5169f77226d4d74d57741786cab89916022780
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-22 16:51:12 -0300

    criado modulo aluno e modulo de rotas alunos

---
+++ src/app/alunos/alunos-routing.module.ts
@@ -0,0 +1,24 @@
+import { AlunosComponent } from './alunos.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+
+const alunosRoutes: Routes = [
+  {
+    path: 'alunos',
+    component: AlunosComponent,
+    children: [
+      { path: 'novo', component: AlunoFormComponent },
+      { path: ':id', component: AlunoDetalheComponent },
+      { path: ':id/editar', component: AlunoFormComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(alunosRoutes)],
+  exports: [RouterModule]
+})
+export class AlunosRoutingModule {}
