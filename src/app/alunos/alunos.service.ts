a7acbbf34660daa979be989af475ef9837295567
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-23 16:10:36 -0300

    criado via Angular CLI

---
+++ src/app/alunos/alunos.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlunosService {
+  private alunos: any[] = [
+    { id: 1, nome: 'aluno 01', email: 'aluno01@email.com' },
+    { id: 2, nome: 'aluno 02', email: 'aluno02@email.com' },
+    { id: 3, nome: 'aluno 03', email: 'aluno03@email.com' },
+    { id: 4, nome: 'aluno 04', email: 'aluno04@email.com' },
+    { id: 5, nome: 'aluno 05', email: 'aluno05@email.com' }
+  ];
+
+  getAlunos() {
+    return this.alunos;
+  }
+
+  getAluno(id: number) {
+    for (let i = 0; i < this.alunos.length; i++) {
+      let aluno = this.alunos[i];
+      if (aluno.id == id) {
+        return aluno;
+      }
+    }
+    return null;
+  }
+
+  constructor() {}
+}
