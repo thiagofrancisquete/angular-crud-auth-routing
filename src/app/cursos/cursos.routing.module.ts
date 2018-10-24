271b5a8121c6b6ef2c04d934132ae8d60ba49576
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-10-24 20:21:59 -0300

    tela de cursos, roteamento e lazy loading do modulo

--- src/app/cursos/cursos.routing.module.ts
+++ src/app/cursos/cursos.routing.module.ts
@@ -6,9 +6,9 @@ import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
 import { CursoNotFoundComponent } from "./curso-not-found/curso-not-found.component";
 
 const cursosRoutes: Routes = [
-  { path: 'cursos', component: CursosComponent},
-  { path: 'curso/:id', component: CursoDetalheComponent},
+  { path: '', component: CursosComponent},
   { path: 'error-404', component: CursoNotFoundComponent},
+  { path: ':id', component: CursoDetalheComponent},
 ]
 
 @NgModule({
