f4aa0b57acc7fb2e2fef7b64415f30e9d6041ad1
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-09-05 17:54:37 -0300

    func listagem de cursos

---
+++ src/app/cursos/cursos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+  getCursos() {
+    return [
+      { id: 1, nome: 'angular 2' },
+      { id: 2, nome: 'react' },
+      { id: 3, nome: 'vue' }
+    ];
+  }
+
+  getCurso( id: number) {
+    let cursos = this.getCursos();
+
+    for(let i=0; i<cursos.length; i++){
+      let curso = cursos[i];
+      if (curso.id == id){
+        return curso;
+      }
+    }
+    return null;
+  }
+
+  constructor() {}
+}
