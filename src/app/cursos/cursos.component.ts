94c8b5186221cd070ac42fc22b7c4017c731eb69
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-09-06 18:24:06 -0300

    utilizando o servico cursos

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  cursos: any[];
+  pagina: number;
+
+  constructor(private cursosService: CursosService) { }
 
   ngOnInit() {
+    this.cursos = this.cursosService.getCursos();
   }
 
 }
+ 
