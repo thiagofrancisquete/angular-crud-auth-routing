b056549f30c0d96d2651464fdec629eb6a73179e
thiagofrancisquete <thiago.francisquete@gmail.com>
2020-05-31 17:20:41 -0300

    add getCurso e getCursos method

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,27 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CursosService {
   getCursos() {
     return [
-      { id: 1, nome: 'angular 2' },
-      { id: 2, nome: 'react' },
-      { id: 3, nome: 'vue' }
+      { id: 1, nome: "angular 2" },
+      { id: 2, nome: "react" },
+      { id: 3, nome: "vue" },
     ];
   }
 
-  getCurso( id: number) {
+  getCurso(id: number) {
     let cursos = this.getCursos();
 
-    for(let i=0; i<cursos.length; i++){
-      let curso = cursos[i];
-      if (curso.id == id){
-        return curso;
-      }
-    }
-    return null;
+    return cursos.find(curso => curso.id == id)
   }
 
   constructor() {}
